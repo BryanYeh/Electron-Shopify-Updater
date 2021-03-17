6d098f0c8aa5a63ac11a4e3e1b16fce127d159e7
BryanYeh <BryanYeh@users.noreply.github.com>
2021-03-16 22:21:56 -0700

    added reading csv

--- routes/update.js
+++ routes/update.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
+const parse = require("csv-parse");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    
-    return res.status(200).send(req.file);
+router.get("/", function (req, res, next) {
+  // Create the parser
+  const parser = parse({columns: true,delimiter: "\t"}, function (err, records) {
+	console.log(records);
+});
+  // Catch any error
+  parser.on("error", function (err) {
+    console.error(err.message);
+  });
+  fs.readdir('./uploads', (err, files) => {
+    files.forEach(file => {
+        fs.createReadStream('./uploads/'+file).pipe(parser);
+    });
+  });
+  
+  return res.status(200).send(req.file);
 });
 
 module.exports = router;
